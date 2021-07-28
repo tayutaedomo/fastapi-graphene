b8eb750a337c6ad09df7e9000fc20858021ab639
tayutaedomo <tayutaedomo@gmail.com>
2021-07-28 12:34:23 +0900

    add: /graphql handling

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import graphene
 from fastapi import FastAPI
+from starlette.graphql import GraphQLApp
+
+from schema import Query
+
 
 app = FastAPI()
+
+app.add_route('/graphql', GraphQLApp(schema=graphene.Schema(query=Query)))
 
 
 @app.get('/')
